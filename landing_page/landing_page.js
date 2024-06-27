ff3a67da8adb9f84f2270821c85c514abed87357
kevin <kevin@kali.kevin>
2024-06-27 17:36:57 +0300

    added attachee page and added updated landing page js

--- landing_page/landing_page.js
+++ landing_page/landing_page.js
@@ -1,8 +1,18 @@
-bioDetailsButton = document.querySelector('bio-details');
+let bioDetailsButtons = document.querySelectorAll('.bio-button');
+let bioDetailsElements = document.querySelectorAll('.bio-details');
 
+function toggleBioDetails(index) {
+    if (bioDetailsElements[index].style.display === 'block') {
+        bioDetailsElements[index].style.display = 'none';
+    } else {
+        bioDetailsElements[index].style.display = 'block';
+    }
+}
 
-function displayBioDetails() {
-   bioElement = bioDetailsBut
-    bioElementadd
-    // addEventListener.
+for (let i = 0; i < bioDetailsButtons.length; i++) {
+    bioDetailsButtons[i].addEventListener('click', (function(i) {
+        return function() {
+            toggleBioDetails(i);
+        }
+    })(i));
 }
